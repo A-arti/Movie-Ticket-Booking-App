5531314be1b094af5e0ad40d94e984702efb9b9b
A-arti <aartikori399@gmail.com>
2025-06-29 23:02:42 +0530

    trailer grid added to home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import { Header } from '../componants/Header'
 import FeaturedSection from '../componants/FeaturedSection'
+import { TrailerSection } from '../componants/TrailerSection'
 
 const Home = () => {
   return (
     <>
     <Header />
     <FeaturedSection />
+    <TrailerSection />
         
     </>
   )
