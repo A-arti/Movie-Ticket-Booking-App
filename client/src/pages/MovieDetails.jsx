6425f75dc59a6c6e9ebf3340677fae088633fece
A-arti <aartikori399@gmail.com>
2025-07-01 22:23:15 +0530

    cards modifeid

--- client/src/pages/MovieDetails.jsx
+++ client/src/pages/MovieDetails.jsx
@@ -1,12 +1,14 @@
 import React, { useEffect, useState } from 'react'
-import { useParams } from 'react-router-dom'
-import { dummyDateTimeData, dummyShowsData } from '../assets/assets'
+import { useNavigate, useParams } from 'react-router-dom'
+import {  dummyDateTimeData, dummyShowsData } from '../assets/assets'
 import { Heart, PlayCircleIcon, StarIcon } from 'lucide-react'
 import BlurCircle from '../componants/BlurCircle'
 import timeFormat from '../lib/timeFormat'
 import DateSelect from '../componants/DateSelect'
+import MovieCard from '../componants/MovieCard'
 
 const MovieDetails = () => {
+  const navigate = useNavigate()
   const {id} = useParams()
   const [show, setShow] = useState(null)
   const getShow = async ()=>{
@@ -58,6 +60,16 @@ const MovieDetails = () => {
         </div>
       </div>
       <DateSelect dateTime={show.dateTime} id={id} />
+      <p className='text-lg font-medium mt-20 mb-8' >You May Also Like</p>
+      <div className='flex flex-wrap max-sm:justify-center gap-8' >
+        {dummyShowsData.slice(0,4).map((movie,index)=>(
+          <MovieCard key={index} movie={movie} />
+        ))}
+
+      </div>
+      <div className='flex justify-center mt-20'>
+        <button onClick={()=>{navigate('/movies'); scrollTo(0,0)}} className='px-10 py-3 text-sm bg-primary hover:bg-primary-dull transition rounded-md font-medium cursor-pointer'>Show more</button>
+      </div>
 
     </div>
   ):(
